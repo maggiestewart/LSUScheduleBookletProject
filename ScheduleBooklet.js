9d7fe517907efce6cddbcdc9e4590c7f872d16ff
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2022-02-22 12:21:16 -0600

    Started ScheduleBooklet.js

---
+++ ScheduleBooklet.js
@@ -0,0 +1,62 @@
+function Course () {
+    this.department = "CSC";
+
+}
+
+let course = new Course();
+
+course.number = 3380;
+course.section = 1;
+course.title = "Object Oriented Design";
+course.hours = 3;
+course.startTime = "10:30";
+course.endTime = "11:50";
+course.days = "TR";
+course.professor = "Aymond";
+course.available = 5;
+course.size = 90;
+course.building = "Patrick Taylor";
+course.room = 1100;
+course.flags = "CI";
+
+class ClassConstruct {
+    //class constructor
+    //will have a function that takes in parameters of data types of the classes (search bars)
+    //will be pulling from XML file
+}
+
+class ClassAggregation {
+
+    Run () {
+        return true;
+    }
+}
+
+class Display {
+    //displays the schedule booklet and such
+    Run () {
+        return true;
+    }
+}
+
+class PopupBox {
+//shows the popup box
+    Run () {
+        return true;
+    }
+
+    box() {
+    }
+}
+
+class SchedulePlanner extends PopupBox {
+}
+
+class HelpfulLinks extends PopupBox {
+}
+
+class SearchBar {
+    Run () {
+        return true;
+    }
+}
