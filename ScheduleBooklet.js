3be050cc16a2dc1bc285b46077f60c5e73229675
Joshua-McCain <98853002+Joshua-McCain@users.noreply.github.com>
2022-02-22 19:38:01 -0600

    Milestone 2 Changes
    PLEASE READ TXT FILE IN SCRATCH BEFORE SEEING CHANGES. PLEASE RUN THIS CODE BEFORE PULLING I DONT KNOW IF IT RUNS!!!

--- ScheduleBooklet.js
+++ ScheduleBooklet.js
@@ -1,15 +1,20 @@
 function Course () {
     this.department = "CSC";
-
 }
 
 let course = new Course();
 
+let objClassAgg = new ClassAggregation();
 
+objClassAgg.fillCourseSelection("ClassFile.xml");
+
+let websiteDisplay = new Display();
+
+websiteDisplay.Run();
 
 
 class ClassConstruct {
-    constructor(file){
+    constructor(){
         course.number = 3380;
         course.section = 1;
         course.title = "Object Oriented Design";
@@ -23,6 +28,9 @@ class ClassConstruct {
         course.building = "Patrick Taylor";
         course.room = 1100;
         course.flags = "CI";
+        course.lab = false;
+        course.labStartTime = null;
+        course.labEndTime = null;
     }
 
     //class constructor
@@ -34,14 +42,32 @@ class ClassConstruct {
 
 class ClassAggregation {
 
-    courseSelection = [];
-    filteredCourseSelection = [];
+    courseSelection;
+    filteredCourseSelection;
+
+    /*
+    * This constructor should take in the courses previously imported from the XML file and fill the both array
+    * members with the data.
+    */
+    constructor(){
+        this.courseSelection = [];
+        this.filteredCourseSelection = [];
+    }
 
     Run () {
         return true;
     }
 
-    sortCourses(number, section, title, hours, startTime, endTime, days, professor, available, size, building, room, flags) {
+    fillCourseSelection(xmlFileName){
+        //The ClassConstruct will be called here for each line in the XML file so that each class is accounted for.
+        // For now, it will just contain the default class.
+        this.courseSelection = [new ClassConstruct()];
+        this.filteredCourseSelection = [new ClassConstruct()];
+    }
+
+    sortCourses(number, section, title, hours, startTime, endTime, days,
+                professor, available, size, building, room, flags, lab, labStart, labEnd) {
+        return true;
     }
 
     sendFilteredClasses(){
@@ -50,8 +76,15 @@ class ClassAggregation {
 }
 
 class Display {
-    //displays the schedule booklet and such
+    /*
+    * displays the schedule booklet and such
+    * Also entails the constant updating of the information being displayed on screen, such as updated classes
+    */
     Run () {
+        //These are examples of the types of search bars and pop-up windows we will instantiate
+        let availabilitySearch = new SearchBar;
+        let schedulePlannerBox = new SchedulePlanner();
+        let helpfulLinksBox = new helpfulLinksBox();
         return true;
     }
 }
@@ -76,5 +109,11 @@ class HelpfulLinks extends PopupBox {
 class SearchBar {
     Run () {
         return true;
+    }
+
+    Search(){
+        objClassAgg.sortCourses(null,null,null,null,null,null,null,
+                                null,null,null,null,null,null,null,
+                                null,null);
     }
 }
