6bb3228765fb24077bd54cb5740cc3ca0ec758c6
Joshua McCain <98853002+Joshua-McCain@users.noreply.github.com>
2022-03-19 18:33:57 -0500

    Completed fillCourseSelection

--- ScheduleBooklet.js
+++ ScheduleBooklet.js
@@ -9,6 +9,7 @@ class ClassConstruct {
 
     #number = null;
     #section = null;
+    #type = null;
     #title = null;
     #hours = null;
     #startTime = null;
@@ -23,10 +24,12 @@ class ClassConstruct {
     #lab = null;
     #labStartTime = null;
     #labEndTime = null;
+    #labDays = null;
 
     static ClassBuilder = class {
         #number = null;
         #section = null;
+        #type = null;
         #title = null;
         #hours = null;
         #startTime = null;
@@ -41,6 +44,7 @@ class ClassConstruct {
         #lab = null;
         #labStartTime = null;
         #labEndTime = null;
+        #labDays = null;
 
         setNumber(number){
             this.#number = number;
@@ -49,6 +53,11 @@ class ClassConstruct {
 
         setSection(section){
             this.#section = section;
+            return this;
+        }
+
+        setType(type){
+            this.#type = type;
             return this;
         }
 
@@ -122,10 +131,16 @@ class ClassConstruct {
             return this;
         }
 
+        setLabDays(labDays){
+            this.#labDays = labDays;
+            return this;
+        }
+
         build() {
             const course = new ClassConstruct(
                 this.#number,
                 this.#section,
+                this.#type,
                 this.#title,
                 this.#hours,
                 this.#startTime,
@@ -139,15 +154,17 @@ class ClassConstruct {
                 this.#flags,
                 this.#lab,
                 this.#labStartTime,
-                this.#labEndTime)
+                this.#labEndTime,
+                this.#labDays)
             return course
         }
     }
 
-    constructor(number, section, title, hours, startTime, endTime, days, professor, available, size, building, room,
-                flags, lab, labStart, labEnd){
+    constructor(number, section, type, title, hours, startTime, endTime, days, professor, available, size, building, room,
+                flags, lab, labStart, labEnd, labDays){
         this.#number = number;
         this.#section = section;
+        this.#type = type;
         this.#title = title;
         this.#hours = hours;
         this.#startTime = startTime;
@@ -162,6 +179,7 @@ class ClassConstruct {
         this.#lab = lab;
         this.#labStartTime = labStart;
         this.#labEndTime = labEnd;
+        this.#labDays = labDays;
     }
 }
 
@@ -201,7 +219,72 @@ class ClassAggregation {
         //Get row tags so that each course can be filled individually
         let rowTags = xmlFile.getElementsByTagName("Row");
 
+
         for (let i = 0; i < rowTags.length; i++) {
+
+            let cellTags = rowTags[i].getElementsByTagName("Cell");
+
+            let tempCourse = new ClassConstruct.ClassBuilder();
+
+            if (cellTags[0].getElementsByTagName("Data")[0].getAttribute("ss.Type") === "Number"
+                || cellTags[0].getElementsByTagName("Data")[0].innerText === "(F)") {
+
+                tempCourse = tempCourse.setAvailable(cellTags[0].getElementsByTagName("Data")[0].innerText)
+                                       .setSize(cellTags[1].getElementsByTagName("Data")[0].innerText)
+                                       .setNumber(cellTags[3].getElementsByTagName("Data")[0].innerText);
+
+                if (cellTags[4].querySelector("Data") != null)
+                    tempCourse = tempCourse.setType(cellTags[4].getElementsByTagName("Data")[0].innerText);
+
+                tempCourse = tempCourse.setSection(cellTags[5].getElementsByTagName("Data")[0].innerText)
+                                       .setTitle(cellTags[6].getElementsByTagName("Data")[0].innerText)
+                                       .setHours(cellTags[7].getElementsByTagName("Data")[0].innerText);
+
+                if (cellTags[8].getElementsByTagName("Data")[0].innerText !== "TBA"){
+
+                    //Set times
+                    const timeArray = cellTags[8].getElementsByTagName("Data")[0].innerText.split("-");
+                    tempCourse = tempCourse.setStartTime(timeArray[0]).setEndTime(timeArray[1])
+                                           .setDays(cellTags[9].getElementsByTagName("Data")[0].innerText);
+
+                    //If it has a room, get the room and building
+                    if (cellTags[10].querySelector("Data") != null)
+                        tempCourse = tempCourse.setRoom(cellTags[10].getElementsByTagName("Data")[0].innerText)
+                                               .setBuilding(cellTags[11].getElementsByTagName("Data")[0].innerText);
+
+                }
+                else
+                    tempCourse = tempCourse.setStartTime("TBA").setEndTime("TBA");
+
+                //Get special tags when applicable
+                if (cellTags[12].querySelector("Data") != null)
+                    tempCourse = tempCourse.setFlags(cellTags[12].getElementsByTagName("Data")[0].innerText);
+
+                //Get professor when applicable
+                if (cellTags[13].querySelector("Data") != null)
+                    tempCourse = tempCourse.setFlags(cellTags[13].getElementsByTagName("Data")[0].innerText);
+
+                //Check to see if the class has a lab and fill out its info if so
+                if (i + 1 < rowTags.length && rowTags[i + 1].getElementsByTagName("Data")[0].innerText === "LAB") {
+                    tempCourse = tempCourse.setLab(true);
+
+                    //Set times by splitting the "-" if not TBA
+                    if (rowTags[i + 1].getElementsByTagName("Data")[1].innerText !== "TBA"){
+                        const labTimeArray = rowTags[i + 1].getElementsByTagName("Data")[1].innerText.split("-");
+                        tempCourse = tempCourse.setLabStartTime(labTimeArray[0]).setLabEndTime(labTimeArray[1])
+                                               .setLabDays(rowTags[i + 1].getElementsByTagName("Data")[2].innerText);
+                    }
+                    else
+                        tempCourse = tempCourse.setLabStartTime("TBA").setLabEndTime("TBA");
+
+                    //We run this line because we want to skip over the lab line we just imported and pick up with the next row
+                    i++;
+                }
+
+                this.courseSelection.push(tempCourse.build());
+            }
+
+            /*
             //Get each Data tag for the row we are working on
             let dataTags = rowTags[i].getElementsByTagName("Data");
 
@@ -216,11 +299,12 @@ class ClassAggregation {
                           .setSize(dataTags[1].innerText)
                           .setNumber(dataTags[3].innerText);
 
-                /* This if statement will split the classes into groups based on the class type, which seems to
-                * be a deciding factor in how a class in structured in the booklet.
-                */
+                //This if statement will split the classes into groups based on the class type, which seems to
+                //be a deciding factor in how a class in structured in the booklet.
                 if (dataTags[4].getAttribute("ss.Type") === "Number"){
-                   tempCourse.setSection(dataTags[4].innerText);
+                   tempCourse.setSection(dataTags[4].innerText)
+                             .setTitle(dataTags[5].innerText)
+                             .setHours(dataTags[6].innerText);
                 }
                 else if (dataTags[4].innerText === "RES" || dataTags[4].innerText === "IND") {
 
@@ -233,11 +317,8 @@ class ClassAggregation {
                     i++;
                 }
 
-            }
+            }*/
         }
-
-        this.courseSelection = [new ClassConstruct()];
-        this.filteredCourseSelection = [new ClassConstruct()];
     }
 
     sortCourses(number, section, title, hours, startTime, endTime, days,
