62fcb9d5e3882d77fb0f2393f1d882fd76fdf1e0
Maggie Stewart <81037163+maggiestewart@users.noreply.github.com>
2022-04-02 18:04:44 -0500

    Added new JavaScript file for the Mock Schedule / Pop Up. Added comments to the file to list ideas for its implementation.

---
+++ SchedulePopUp.js
@@ -0,0 +1,7 @@
+// File for the Mock Schedule aka the Pop-up box for the user's proposed schedule
+
+// take user input for class department, number, section number, and credit hours
+// format and output the user input in a pop up box / window
+
+// ask for user input for desired number of credit hours
+// loop until user hits 'q' or exceeds desired number of credit hours for courses selected
